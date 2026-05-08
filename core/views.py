378cf8c951e4c08da59e670fb643a5c11b9f7e0e
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-05-08 12:17:43 +0530

    puddle up to date with items and categories

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from item.models import Item, Category
+
+def index(request):
+    items = Item.objects.filter(is_sold=False).order_by('-created_at')[0:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'items': items,
+        'categories': categories,
+        })
+
+def contact(request):
+    return render(request, 'core/contact.html')
+# Create your views here.
