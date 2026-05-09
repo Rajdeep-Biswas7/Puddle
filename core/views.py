e597970934317deda2168cd6b8bf178aeeae3e14
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-05-09 23:54:12 +0530

    create form and signup system

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from item.models import Item, Category
-
+from .forms import SignUpForm
 def index(request):
     items = Item.objects.filter(is_sold=False).order_by('-created_at')[0:6]
     categories = Category.objects.all()
@@ -12,3 +12,8 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
 # Create your views here.
+def signup(request):
+    form = SignUpForm()
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
