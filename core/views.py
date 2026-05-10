7c7dc47d9b2d3bf2f0918a2c26202d21e5e0ea86
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-05-10 23:46:42 +0530

    Implement signup functionality with form handling and update navigation link

--- core/views.py
+++ core/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Item, Category
 from .forms import SignUpForm
+
 def index(request):
     items = Item.objects.filter(is_sold=False).order_by('-created_at')[0:6]
     categories = Category.objects.all()
     return render(request, 'core/index.html', {
         'items': items,
         'categories': categories,
-        })
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
-# Create your views here.
+
+# Updated signup view to handle form submissions
 def signup(request):
-    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('core:index')
+    else:
+        form = SignUpForm()
+
     return render(request, 'core/signup.html', {
         'form': form
     })
