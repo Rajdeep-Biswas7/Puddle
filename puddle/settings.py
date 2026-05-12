ca32b33e097e9ba463718b06e2baeb938dc40014
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-05-12 23:48:57 +0530

    Add authentication links to navigation and update login form styling

--- puddle/settings.py
+++ puddle/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-qjphsr3d^rk8!omd)2gn-yar)8&8qk$5%l9u0e^h2$(x)p@+98
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+LOGIN_URL='/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
 
